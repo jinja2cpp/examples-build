5ecb3ffa8c2a37d48ea15a932752a3b0837305e9
Flex Ferrum <flexferrum@gmail.com>
2019-05-07 23:46:30 +0300

    Add test for 'internal' deps management mode with Jinja2Cpp installation

---
+++ external/internal/build.bat
@@ -0,0 +1,11 @@
+mkdir .jinja2cpp-build
+mkdir .jinja2cpp-install
+cd .jinja2cpp-build
+cmake ..\..\..\thirdparty\Jinja2Cpp -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=..\.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF
+cmake --build . --config Release --target install
+cd ..
+mkdir .build
+cd .build
+cmake ..\ -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -Djinja2cpp_DIR=..\.jinja2cpp-install\lib\jinja2cpp
+cmake --build . --config Release
+
