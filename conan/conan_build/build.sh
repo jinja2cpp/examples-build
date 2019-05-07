19b5d11cdd558f7b9534064201b8db8d111105d9
Flex Ferrum <flexferrum@gmail.com>
2019-05-07 15:03:08 +0300

    Fix build of 'conan-build' case

--- conan/conan_build/build.sh
+++ conan/conan_build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir .build && cd .build
-conan install  --build=missing ..
+conan install  --build=missing -s compiler.version=5 -s compiler.libcxx=libstdc++11 ..
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
