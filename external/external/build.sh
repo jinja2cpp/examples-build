407966a0a6239b806b05b9d8d6b7d786e4871bdc
Flex Ferrum <flexferrum@gmail.com>
2019-05-10 19:40:38 +0300

    Fix build for some scenarios

--- external/external/build.sh
+++ external/external/build.sh
@@ -5,5 +5,5 @@ set CMAKE_PREFIX_PATH=../../../thirdparty/expected-lite;../../../thirdparty/vari
 cmake ../../../thirdparty/Jinja2Cpp -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF -DJINJA2CPP_DEPS_MODE=external
 cmake --build . --target install
 cd .. && mkdir .build && cd .build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -Djinja2cpp_DIR=../.jinja2cpp-install/lib/jinja2cpp ..
 cmake --build .
--- external/external_boost/build.sh
+++ external/external_boost/build.sh
@@ -4,5 +4,5 @@ mkdir .jinja2cpp-build && cd .jinja2cpp-build
 cmake ../../../thirdparty/Jinja2Cpp -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF -DJINJA2CPP_DEPS_MODE=external-boost
 cmake --build . --target install
 cd .. && mkdir .build && cd .build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -Djinja2cpp_DIR=../.jinja2cpp-install/lib/jinja2cpp ..
 cmake --build .
