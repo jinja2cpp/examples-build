4fc73ea55b482ed6044997f1d23108d94ca3c5ea
Flex Ferrum <flexferrum@gmail.com>
2019-05-10 19:50:10 +0300

    Fix 'external' deps mode test build

--- external/external/build.sh
+++ external/external/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 mkdir .jinja2cpp-build && cd .jinja2cpp-build
-set CMAKE_PREFIX_PATH=../../../thirdparty/expected-lite;../../../thirdparty/variant-lite;../../../thirdparty/optional-lite;../../../thirdparty/value-ptr-lite
-cmake ../../../thirdparty/Jinja2Cpp -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF -DJINJA2CPP_DEPS_MODE=external
+#set CMAKE_PREFIX_PATH=../../../thirdparty/expected-lite;../../../thirdparty/variant-lite;../../../thirdparty/optional-lite;../../../thirdparty/value-ptr-lite
+cmake ../../../thirdparty/Jinja2Cpp -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF -DJINJA2CPP_DEPS_MODE=external -DCMAKE_PREFIX_PATH="../../../thirdparty/expected-lite;../../../thirdparty/variant-lite;../../../thirdparty/optional-lite;../../../thirdparty/value-ptr-lite"
 cmake --build . --target install
 cd .. && mkdir .build && cd .build
 cmake -DCMAKE_BUILD_TYPE=Release -Djinja2cpp_DIR=../.jinja2cpp-install/lib/jinja2cpp ..
