321ce658238184060696d80c9481f3464a794e8c
Flex Ferrum <flexferrum@gmail.com>
2019-06-02 00:50:05 +0300

    Fix build

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ TEST(BasicTests, PlainSingleLineTemplateProcessing)
     Template tpl;
     ASSERT_TRUE(!!tpl.Load(source));
 
-    std::string result = tpl.RenderAsString(ValuesMap{});
+    std::string result = tpl.RenderAsString(ValuesMap{}).value();
     std::cout << result << std::endl;
     std::string expectedResult = "Hello World from Parser!";
     EXPECT_STREQ(expectedResult.c_str(), result.c_str());
