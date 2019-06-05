a76d5f8a8a38c16e42eaa5f4bbdccd1ae5bde564
Flex Ferrum <flexferrum@gmail.com>
2019-06-05 10:31:14 +0300

    Fix build with conan package 0.9.1

--- conan/0.9.1/main.cpp
+++ conan/0.9.1/main.cpp
@@ -1,1 +1,21 @@
-#include "../../src/main.cpp"
+#include <iostream>
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include <jinja2cpp/template.h>
+
+using namespace jinja2;
+
+TEST(BasicTests, PlainSingleLineTemplateProcessing)
+{
+    std::string source = "Hello World from Parser!";
+
+    Template tpl;
+    ASSERT_TRUE(!!tpl.Load(source));
+
+    std::string result = tpl.RenderAsString(ValuesMap{});
+    std::cout << result << std::endl;
+    std::string expectedResult = "Hello World from Parser!";
+    EXPECT_STREQ(expectedResult.c_str(), result.c_str());
+}
