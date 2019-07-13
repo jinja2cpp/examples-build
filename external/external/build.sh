381c1d48b2f6a937196382f18bdd8e6b3fac4c24
Flex Ferrum <flexferrum@gmail.com>
2019-07-13 17:29:01 +0300

    Fix build

--- external/external/build.sh
+++ external/external/build.sh
@@ -3,7 +3,7 @@
 SRC_DIR=`pwd`
 mkdir -p .jinja2cpp-build && cd .jinja2cpp-build
 echo $SRC_DIR
-cmake ../../../thirdparty/Jinja2Cpp -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF -DJINJA2CPP_DEPS_MODE=external -DCMAKE_PREFIX_PATH="$SRC_DIR/../../thirdparty/expected-lite;$SRC_DIR/../../thirdparty/variant-lite;$SRC_DIR/../../thirdparty/optional-lite;$SRC_DIR/../../thirdparty/value-ptr-lite"
+cmake ../../../thirdparty/Jinja2Cpp -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF -DJINJA2CPP_DEPS_MODE=external -DCMAKE_PREFIX_PATH="$SRC_DIR/../../thirdparty/expected-lite;$SRC_DIR/../../thirdparty/variant-lite;$SRC_DIR/../../thirdparty/optional-lite;$SRC_DIR/../../thirdparty/string-view-lite;$SRC_DIR/../../thirdparty/fmt"
 cmake --build . --target install
 cd .. && mkdir -p .build && cd .build
 cmake -DCMAKE_BUILD_TYPE=Release -Djinja2cpp_DIR=../.jinja2cpp-install/lib/jinja2cpp ..
