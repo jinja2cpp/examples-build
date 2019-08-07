3443ae3f9ee2930a75085918026a4dcec979726e
Flex Ferrum <flexferrum@gmail.com>
2019-08-07 22:34:38 +0300

    Fix build issues

--- external/external/build.bat
+++ external/external/build.bat
@@ -1,7 +1,7 @@
 mkdir .jinja2cpp-build
 mkdir .jinja2cpp-install
 cd .jinja2cpp-build
-set CMAKE_PREFIX_PATH=../../../thirdparty/expected-lite;../../../thirdparty/variant-lite;../../../thirdparty/optional-lite;../../../thirdparty/string-view-lite;../../../thirdparty/fmt;../../../thirdparty/robin-hood-hashing
+set CMAKE_PREFIX_PATH=../../../thirdparty/expected-lite;../../../thirdparty/variant-lite;../../../thirdparty/optional-lite;../../../thirdparty/string-view-lite;../../../thirdparty/fmt;../../../thirdparty/robin-hood-hashing/src;
 cmake ..\..\..\thirdparty\Jinja2Cpp -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=..\.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF -DJINJA2CPP_DEPS_MODE=external
 cmake --build . --config Release --target install
 cd ..
