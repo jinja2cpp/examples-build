41c47d23d4b191e128e7a68037dfae873ac83978
Flex Ferrum <flexferrum@gmail.com>
2019-10-08 00:00:32 +0300

    Update thirdparty submodules

--- external/internal/build.bat
+++ external/internal/build.bat
@@ -1,7 +1,7 @@
 mkdir .jinja2cpp-build
 mkdir .jinja2cpp-install
 cd .jinja2cpp-build
-cmake ..\..\..\thirdparty\Jinja2Cpp -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=..\.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF
+cmake ..\..\..\thirdparty\Jinja2Cpp -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=..\.jinja2cpp-install -DJINJA2CPP_BUILD_TESTS=OFF -DJINJA2CPP_DEPS_MODE=internal
 cmake --build . --config Release --target install
 cd ..
 mkdir .build
